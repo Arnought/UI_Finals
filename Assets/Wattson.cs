1d06140063c59aa86d9cc366dbdc561eb4c8c418
cicerolb <115563856+cicerolb@users.noreply.github.com>
2023-09-29 15:47:02 +0800

    DEAD

---
+++ Assets/Wattson.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wattson : MonoBehaviour
+{
+    public Animator anim;
+    bool down = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Down() 
+    { 
+        anim.SetBool
+    }
+}
